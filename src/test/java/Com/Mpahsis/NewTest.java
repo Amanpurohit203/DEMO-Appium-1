81d00fc779299f408ec68244c8d41a2cb8e8fef3
aman <purohitaman203@gmial.com>
2022-03-15 09:18:35 +0530

    First commit

---
+++ src/test/java/Com/Mpahsis/NewTest.java
@@ -0,0 +1,19 @@
+package Com.Mpahsis;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void AmaZON() {
+  
+  System.out.println("Amazon ");}
+
+@Test
+public void Mobile() {
+	System.out.println("Hello");
+	System.out.println("aman aman aman");
+}
+@Test
+public void Andriod() {
+	System.out.println(10+20);
+}}
---
+++ src/test/java/Com/Mpahsis/NewTest2.java
@@ -0,0 +1,16 @@
+package Com.Mpahsis;
+
+import org.testng.annotations.Test;
+
+public class NewTest2 {
+  @Test
+  public void Flipkart() {
+	  System.out.println("Filipkarty");
+  }
+  @Test
+  public void Google() {
+	  System.out.println("Hello google");
+	  System.out.println("namana");
+	  
+  }
+}
